ba4a708131fe0c3983b3281f8d0e7606dd19f9da
ekov22 <122702192+ekov22@users.noreply.github.com>
2023-08-26 00:08:04 +0300

    Update code.js

--- code.js
+++ code.js
@@ -1,2 +1,26 @@
 
 //alert("woah");
+
+function fnBrowserDetect(){
+                 
+         let userAgent = navigator.userAgent;
+         let browserName;
+         
+         if(userAgent.match(/chrome|chromium|crios/i)){
+             browserName = "chrome";
+           }else if(userAgent.match(/firefox|fxios/i)){
+             browserName = "firefox";
+           }  else if(userAgent.match(/safari/i)){
+             browserName = "safari";
+           }else if(userAgent.match(/opr\//i)){
+             browserName = "opera";
+           } else if(userAgent.match(/edg/i)){
+             browserName = "edge";
+           }else{
+             browserName="No browser detection";
+           }
+         
+          document.querySelector("h1").innerText="You are using "+ browserName +" browser";         
+  }
+
+console.log("why do u use " + fnBrowserDetect");
